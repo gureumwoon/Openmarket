86f591f8c83d7d99a5a32c464be7d1c1753ced9c
gureumwoon <vfrvfr3221@gmail.com>
2022-08-05 01:01:32 +0900

    빈 카트 UI 완성

---
+++ src/components/RadioCheck.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import styled from "styled-components";
+
+function RadioCheck(props) {
+    const { width, margin } = props
+    const styles = { width, margin }
+    return (
+        <Radio {...styles}>
+            <input type="radio" />
+        </Radio>
+    )
+}
+
+const Radio = styled.div`
+    width: ${(props) => props.width};
+    margin: ${(props) => props.margin};
+    input[type='radio'] {
+    appearance: none;
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    border:2px solid #21BF48 ;
+    position: relative;
+    &:checked::after {
+        display: inline-block;
+        content: "";
+        width: 12px;
+        height: 12px;
+        background-color: #21BF48;
+        border-radius: 50%;
+        position: absolute;
+        top: 2px;
+        right: 2px;
+    }
+}
+`
+
+
+export default RadioCheck
--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -3,13 +3,70 @@ import styled from "styled-components";
 
 // components
 import Nav from '../components/Nav'
+import RadioCheck from '../components/RadioCheck';
 
 function ShoppingCart() {
     return (
         <div>
             <Nav user_nav />
+            <Main>
+                <h1>장바구니</h1>
+                <div className='cart-nav'>
+                    <RadioCheck margin="0 0 0 30px" />
+                    <p>상품정보</p>
+                    <p>수량</p>
+                    <p>상품금액</p>
+                </div>
+                <div className='empty-cart'>
+                    <p>장바구니에 담긴 상품이 없습니다.</p>
+                    <p>원하는 상품을 장바구니에 담아보세요!</p>
+                </div>
+            </Main>
         </div>
     )
 }
+
+const Main = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    h1 {
+        font-size: 36px;
+        margin: 54px 0 52px;
+    }
+    .cart-nav {
+        width: 1280px;
+        height: 60px;
+        background-color: #F2F2F2;
+        border-radius: 10px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        p {
+            font-size: 18px;
+            &:nth-child(4) {
+                margin-right: 131px;
+            }
+        }
+    }
+    .empty-cart {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        p {
+            &:first-child {
+                margin: 200px 0 17px;
+                font-size: 18px;
+                font-weight: bold;
+            }
+            &:nth-child(2) {
+                font-size: 14px;
+                color: #767676;
+            }
+        }
+    }
+`
 
 export default ShoppingCart
