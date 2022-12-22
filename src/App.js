08cd58aa5d82f52458292e2d15eedde0d5052598
gureumwoon <vfrvfr3221@gmail.com>
2022-12-23 02:28:25 +0900

    FEAT: search 페이지 queryString 생성 완료

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
         <Route path="/detail/:id" element={<ProductDetail />} />
         <Route path="/cart" element={<ShoppingCart itemCount={itemCount} />} />
         <Route path="/payment" element={<Payment />} />
-        <Route path={['/search', '/info']} element={<Search />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
     </div>
   );
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,14 @@
+import queryString from 'query-string'
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 import Nav from '../components/Nav'
 
 function Search() {
     const userType = localStorage.getItem("type")
     const isLogin = localStorage.getItem("token")
-
+    // const { search } = useLocation();
+    // const values = queryString.parse(search)
+    // console.log(values)
     return (
         <div>
             {userType === "SELLER" ?
